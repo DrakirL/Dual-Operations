57dc8c13a917c3ad717db41e0b0917af690be2dd
DrakirL <rikard.nt.larsson@hotmail.com>
2020-03-30 16:59:05 +0200

    Super simple test
    Added super simple test

---
+++ Assets/Scenes/MoveTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed, transform.position.y, transform.position.z);
+    }
+}
