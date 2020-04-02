f22241e32df2bc2f279dd6ea34ecdf0cdbbfeacc
Ekkara <47211128+Ekkara@users.noreply.github.com>
2020-04-02 13:56:03 +0200

    Camera
    now works in terms of only sending signals instead of each frame, also fixed distance instead of entire camera view

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -8,18 +8,22 @@ public struct CameraStruct
 {
     public CameraScript camera;
     public RenderTexture cameraView;
+    public bool AgentIsInCamera;
 }
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] CameraStruct[] cameraStruct;
+
+    //OBS this is not ment to be public, remove when game is up
+    [SerializeField] public float cameraFOVDistance;
+    [SerializeField] float cameraAlertTime;
     [SerializeField] GameObject Spy;
     Collider spyCol;
-   
+
     [SerializeField] float shutDownTimer = 10;
     [SerializeField] RawImage cameraRenderer;
     [SerializeField] int tempImageChanger = 0;
-
     
     // Start is called before the first frame update
     void Start()
@@ -32,76 +36,42 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        //example to see if the spy is detected
-        // Debug.Log(isSpySeenByAnyCamera(cameras));
-        Debug.Log(isAgentSeenByThisCamera(cameraStruct[0]));
-
-        //example how to see all the cameras that know where the spy is located
-        /* List<GameObject> camerasThatSeeTheSpyGO = camerasThatSeeTheSpy(cameras);
-        for(int i = 0; i < camerasThatSeeTheSpyGO.Count; i++)
+        List<CameraScript> alertTimes = new List<CameraScript>();
+        alertTimes = camerasThatSeeTheSpy(cameraStruct);
+        for (int i = 0; i < alertTimes.Count; i++)
         {
-            Debug.Log(camerasThatSeeTheSpyGO[i].name + " detect the spy");
-        }*/
-
-        //cameraRenderer.texture = getCameraView(cameras, 0);
-        //cameraRenderer.texture = getCameraView(cameras, tempImageChanger);
-        
-        
-    
-    }
-
-    RenderTexture updateHackerCameraView(int index)
-    {
-        return cameraStruct[index].cameraView;
-        // cameraRenderer.texture = cameraStruct[index].cameraView;
-    }
-    bool isAgentSeenByThisCamera(CameraStruct[] TracingCameras, int index)
-    {
-        return TracingCameras[index].camera.isObjectVisible(Spy, spyCol);
-    }
-    bool isAgentSeenByThisCamera(CameraStruct TracingCamera)
-    {
-        return TracingCamera.camera.isObjectVisible(Spy, spyCol);
-    }
-    bool isAgentSeenByAnyCamera(CameraStruct[] TracingCameras)
-    {
-        for (int i = 0; i < TracingCameras.Length; i++)
-        {
-            if (TracingCameras[i].camera.isObjectVisible(Spy, spyCol))
-                return true;
+            //TYPE HERE WHAT SHOULD HAPPEN WHEN CAMERA DETECT AGENT
+            //Debug.Log(alertTimes[i].gameObject.name);
         }
-        return false;
     }
-    RenderTexture getCameraView(CameraStruct[] TracingCameras, int index)
+
+    //functions that is currently not being used
+    /* bool isAgentSeenByThisCamera(CameraStruct[] TracingCameras, int index)
+     {
+         return TracingCameras[index].camera.isObjectVisible(Spy, spyCol, cameraFOVDistance, cameraAlertTime);
+     }
+     bool isAgentSeenByThisCamera(CameraStruct TracingCamera)
+     {
+         return TracingCamera.camera.isObjectVisible(Spy, spyCol, cameraFOVDistance, cameraAlertTime);
+     }
+
+     bool isAgentSeenByAnyCamera(CameraStruct[] TracingCameras)
+     {
+         for (int i = 0; i < TracingCameras.Length; i++)
+         {
+             if (TracingCameras[i].camera.isObjectVisible(Spy, spyCol, cameraFOVDistance, cameraAlertTime))
+                 return true;
+         }
+         return false;
+     }*/
+    //function to detect all cameras that can see the player
+    List<CameraScript> camerasThatSeeTheSpy(CameraStruct[] TracingCameras)
     {
-        /*   Camera camera = cameras[index].gameObject.GetComponent<Camera>();
-
-           // Make a new texture and read the active Render Texture into it.
-           Texture2D image = new Texture2D(camera.pixelWidth, camera.pixelHeight);
-           image.ReadPixels(new Rect(0, 0, camera.pixelWidth, camera.pixelWidth), 0, 0);
-           image.Apply();
-
-           //  Sprite sprite = Sprite.Create(image, new Rect(0.0f, 0.0f, image.width, image.height), new Vector2(0.5f, 0.5f), 100.0f);
-
-           if (TracingCameras[index].cameraActive)
-           {
-               return image;
-           }
-           Debug.LogWarning("camera is not active");
-           return null;
-
-       //    return cameras[index].gameObject.GetComponent<Camera>().activeTexture;
-       */
-       return TracingCameras[index].camera.gameObject.GetComponent<Camera>().targetTexture;
-    }
-    List<GameObject> camerasThatSeeTheSpy(CameraStruct[] TracingCameras)
-    {
-        List<GameObject> tempList = new List<GameObject>(); 
+        List<CameraScript> tempList = new List<CameraScript>();
         for (int i = 0; i < TracingCameras.Length; i++)
         {
-            if (TracingCameras[i].camera.isObjectVisible(Spy, spyCol))
-                tempList.Add(TracingCameras[i].camera.gameObject);
+            if (TracingCameras[i].camera.isObjectVisible(Spy, spyCol, cameraFOVDistance, cameraAlertTime))
+                tempList.Add(TracingCameras[i].camera);
         }
         return tempList;
     }
@@ -109,20 +79,29 @@ public class CameraManager : MonoBehaviour
 
 
 
- 
 
-    public void shutDownCamera(CameraStruct camereStruct)
+    //hacker funktioanlaties
+    RenderTexture updateHackerCameraView(int index)
     {
-        if (camereStruct.camera.cameraActive)
-        {
-            camereStruct.camera.cameraActive = false;
-            StartCoroutine(acivateCamera(camereStruct, shutDownTimer));
-        }
+        return cameraStruct[index].cameraView;
+        // cameraRenderer.texture = cameraStruct[index].cameraView;
+    }
+    RenderTexture getCameraView(CameraStruct[] TracingCameras, int index)
+    {
+        return TracingCameras[index].camera.gameObject.GetComponent<Camera>().targetTexture;
     }
 
-    private IEnumerator acivateCamera(CameraStruct camreStruct, float waitTime)
+    public void shutDownCamera(CameraStruct cameraStruct)
+    {
+        if (cameraStruct.camera.cameraActive)
+        {
+            cameraStruct.camera.cameraActive = false;
+            StartCoroutine(acivateCamera(cameraStruct, shutDownTimer));
+        }
+    }
+    private IEnumerator acivateCamera(CameraStruct cameraStruct, float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        camreStruct.camera.cameraActive = true;
+        cameraStruct.camera.cameraActive = true;
     }
 }
--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+
+    [SerializeField] CameraManager removeMe;
     Camera thisCamera;
     Plane[] planes;
-    /*[HideInInspector]*/ public bool cameraActive;
+    [HideInInspector] public bool cameraActive;
+    [HideInInspector] public float detectedTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +22,41 @@ public class CameraScript : MonoBehaviour
         }
         planes = GeometryUtility.CalculateFrustumPlanes(thisCamera);
     }
-    public bool isObjectVisible(GameObject objectToFind, Collider objectCollider)
+    public bool isObjectVisible(GameObject objectToFind, Collider objectCollider, float distance, float timeBeforeDetected)
     {
         if (cameraActive)
         {
-            RaycastHit objectHit;
-            Vector3 direction = Vector3.Normalize(objectToFind.transform.position - transform.position);
-            Physics.Raycast(transform.position, direction, out objectHit);
+            if (Vector3.Distance(objectToFind.transform.position, this.transform.position) <= distance)
+            {
+                RaycastHit objectHit;
+                Vector3 direction = Vector3.Normalize(objectToFind.transform.position - transform.position);
+                Physics.Raycast(transform.position, direction, out objectHit);
 
-            return GeometryUtility.TestPlanesAABB(planes, objectCollider.bounds) && objectHit.transform.gameObject == objectToFind;
+                if (GeometryUtility.TestPlanesAABB(planes, objectCollider.bounds) && objectHit.transform.gameObject == objectToFind)
+                {
+                    detectedTime += Time.deltaTime;
+                    if (detectedTime >= timeBeforeDetected)
+                    {
+                        detectedTime = 0;
+                        return true;
+                    }
+                }
+                else
+                {
+                    detectedTime = 0;
+                }
+            }
+            else
+            {
+                detectedTime = 0;
+            }
         }
         return false;
+    }
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, removeMe.cameraFOVDistance);
     }
 }
