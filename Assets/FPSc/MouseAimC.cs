166c40fa84152fbd377ad40bebec56899e5cf94d
FuyukiAndo <47009923+FuyukiAndo@users.noreply.github.com>
2020-04-14 10:42:26 +0200

    fixed collision

--- Assets/FPSc/MouseAimC.cs
+++ Assets/FPSc/MouseAimC.cs
@@ -29,12 +29,23 @@ public class MouseAimC : MonoBehaviour
 	
 	public LayerMask colMask;
 	
-	private CharacterController controller; //Plan B
 	private bool isGrounded = false;
 	public float slopeRise = 1f;
 	public float slopeFall = 1f;
 	public float height = 0.5f;
-	public float posRecover = 5f;
+	public float jumpSpeed = 10f;
+	
+
+	float posRecover = 10f;
+	
+	
+	public enum MouseAimStyle
+	{
+		Quake,
+		WolfensteinDoom
+	};
+	
+	public MouseAimStyle mouseAimStyle;
 	
     void Start()
     {
@@ -49,10 +60,6 @@ public class MouseAimC : MonoBehaviour
 		
 		Cursor.lockState = CursorLockMode.Locked;
 		cameraView.transform.position = new Vector3(transform.position.x,transform.position.y+cameraOffsetY,transform.position.z);		
-		
-		
-		controller = GetComponent<CharacterController>();
-		
     }
 	
 	void MoveWalk(float forward,float right)
@@ -72,7 +79,7 @@ public class MouseAimC : MonoBehaviour
 		movementDirection.x += moveSpeed * movdir.x;
 		movementDirection.z += moveSpeed * movdir.z;
 		
-		movementDirection.y += -gravity;
+
 		
 		//Add jump action here
 	}
@@ -137,10 +144,18 @@ public class MouseAimC : MonoBehaviour
 		int inv = Convert.ToInt32(inverted == true ? 1:-1);
 		
 		rot.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
-		rot.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+		
+		if(mouseAimStyle == MouseAimStyle.WolfensteinDoom)
+		{
+			rot.y = 0;
+		}
+		else 
+		{
+			rot.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
-		//Clamp y-axis view
-		rot.y = Mathf.Clamp(rot.y,viewLimit.x,viewLimit.y);
+			//Clamp y-axis view
+			rot.y = Mathf.Clamp(rot.y,viewLimit.x,viewLimit.y);
+		}
 		
 		cameraView.transform.eulerAngles = new Vector2(0,rot.x);
 		
@@ -152,273 +167,193 @@ public class MouseAimC : MonoBehaviour
 	}
 
 	void Collision(ref Vector3 movement, float forward,float right)
-	{
-		//float distY = GetComponent<BoxCollider>().bounds.extents.y;
-		//float distZ = GetComponent<BoxCollider>().bounds.extents.z;
-		//float distX = GetComponent<BoxCollider>().bounds.extents.x;
-		float dist = GetComponent<SphereCollider>().radius;
-		
-		Ray downRay =  new Ray (transform.position, -transform.up);
-		Ray frontRay =  new Ray (transform.position, transform.forward);
-				
-		//Ray downRay2 =  new Ray (transform.position, -transform.up);
-		Ray upRay =  new Ray (transform.position, transform.up);
-		
-
-		Ray backRay = new Ray (transform.position, -transform.forward);
-		
-		Ray rightRay = new Ray (transform.position, transform.right);
-		Ray leftRay = new Ray (transform.position, -transform.right);
-		
+	{		
 		RaycastHit hit;
+		float dist = (GetComponent<SphereCollider>().radius)*0.5f;
 		
+		Ray downRay =  new Ray (transform.position, Vector3.down);
+		Ray upRay =  new Ray (transform.position, Vector3.up);
+		Ray upForwardRay =  new Ray (transform.position, Vector3.up+(forward*transform.forward));
+		
+		Ray frontRay =  new Ray (transform.position, forward*transform.forward);
+		Ray rightRay = new Ray (transform.position, right*transform.right);
+		Ray uRightRay = new Ray (transform.position, transform.right);
+		Ray uLeftRay = new Ray (transform.position, -transform.right);
+		//Ray frontRightRay = new Ray (transform.position, right*transform.right+forward*transform.forward);
+		
+		//Ray backRay =  new Ray (transform.position, -transform.forward);
+		//Ray leftRay = new Ray (transform.position, transform.right);
+		
+		Debug.DrawLine(downRay.origin,downRay.origin + downRay.direction * dist, Color.green);
+		Debug.DrawLine(rightRay.origin,rightRay.origin + rightRay.direction * dist, Color.red);	
 
-		
-		/*//poor slope collision detection, needs to be improved
-		if(Physics.Raycast(downRay2, out hit, distY, colMask))	
-		{
-				float n2 = (Vector3.Dot(hit.normal,Vector3.up));
-				float ncos = Mathf.Acos(n2);
-				//float nsin = Mathf.Asin(n2);
-				float ang = (Mathf.Rad2Deg*ncos);
-				
-				//example test with 45 deg
-				if(ang <= 45 && ang != 0)
-				{
-					//Calculate cross product to get normal 
-					Vector3 t = Vector3.Cross(hit.normal, transform.forward);
-					Vector3 velocity = Vector3.Cross(t,hit.normal).normalized;
-					
-					if(forward > 0|| forward < 0)
-					{ 
-						movement.y = hit.point.y*slopeRise;// + velocity.y*moveSpeed*slopeRise;
-					}
-				}
-			
-		}*/
-		
+
 		//Checking below player for surface
-		if(Physics.Raycast(downRay, out hit, dist + 0.05f, colMask))
+		//&& Physics.Raycast(down2Ray, out hit, dist + 0.05f, colMask)
+		if(Physics.Raycast(downRay, out hit, dist + 0.05f, colMask,QueryTriggerInteraction.Ignore))
 		{
 			MeshCollider meshCollider = hit.collider as MeshCollider;
-			if (meshCollider == null || meshCollider.sharedMesh == null)
-			{
-				return;
-			}
+			Mesh mesh = meshCollider.sharedMesh;
+			Vector3[] v = mesh.vertices;
 			
+			float n2 = (Vector3.Dot(hit.normal,Vector3.up));
+			float ncos = Mathf.Acos(n2);
+			float ang = (Mathf.Rad2Deg*ncos);	
+			
+			for(int i = 0; i < v.Length;i++)
+			{
+				float n = Vector3.Dot(v[i] - movementDirection, hit.normal);
+			
+				if(n >= 0.01f) 
+				{
+					//Height correction with linear interpolation if clipping with mesh's face
+					if(hit.distance < dist)
+					{
+						transform.position = Vector3.Lerp(transform.position, hit.point + Vector3.up * dist, posRecover * Time.fixedDeltaTime);
+					}
+					
+					isGrounded = true;
+					movementDirection.y = 0;
+
+					break;
+				}
+			}
+		}
+		
+		if(Physics.Raycast(upRay, out hit, dist + 0.05f, colMask,QueryTriggerInteraction.Ignore))
+		{
+			MeshCollider meshCollider = hit.collider as MeshCollider;
 			Mesh mesh = meshCollider.sharedMesh;
 			Vector3[] v = mesh.vertices;
 			
 			for(int i = 0; i < v.Length;i++)
 			{
 				float n = Vector3.Dot(v[i] - movementDirection, hit.normal);
-	
-				if(n > 0.0f)
+			
+				if(n > 0.01f) 
 				{
-					//Correct the fricking thing from moving into things
-					if(hit.distance < dist) //Vector3.Distance(transform.position,hit.point)
-					{
-						transform.position = Vector3.Lerp(transform.position, hit.point + Vector3.up * dist, posRecover * Time.fixedDeltaTime);
-						//transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up * dist, 5f * Time.fixedDeltaTime);
-					}
-					//Source for solution (finally)
-					//https://www.youtube.com/watch?v=98MBwtZU2kk
 					
-					movementDirection.y = 0;
+					//Height correction with linear interpolation if clipping with mesh's face
+					if(hit.distance < dist)
+					{
+						transform.position = Vector3.Lerp(transform.position, hit.point + Vector3.down * dist, posRecover * Time.fixedDeltaTime);
+						movementDirection.y = 0;
+					}
+					
+					isGrounded = false;		
 					break;
 				}
 			}
 		}
 		
-
-
-		
-		if(forward > 0f)
-		{
-			Debug.DrawLine(frontRay.origin,Vector3.forward, Color.red);
-			
-			if(Physics.Raycast(frontRay, out hit, dist, colMask))
-			{
-				MeshCollider meshCollider = hit.collider as MeshCollider;
-				if (meshCollider == null || meshCollider.sharedMesh == null)
-				{	
-					return;
-				}
-				
-				Mesh mesh = meshCollider.sharedMesh;
-				Vector3[] v = mesh.vertices;
-			
-				for(int i = 0; i < v.Length;i++)
-				{
-					float n = Vector3.Dot(v[i] - movementDirection, hit.normal);
-		
-					if(n > 0.0f || n < 0.0f)
-					{
-						movementDirection.z = 0;
-						break;
-					}
-				}
-			}
-		}
-		
-		
-		/*if(Physics.Raycast(upRay, out hit, distY, colMask))
+		if(Physics.Raycast(upForwardRay, out hit, dist + 0.05f, colMask,QueryTriggerInteraction.Ignore))
 		{
 			MeshCollider meshCollider = hit.collider as MeshCollider;
-			if (meshCollider == null || meshCollider.sharedMesh == null)
-			{
-				return;
-			}
-			
 			Mesh mesh = meshCollider.sharedMesh;
 			Vector3[] v = mesh.vertices;
 			
-		 	//Debug.DrawLine(downRay.origin,transform.up, Color.red);
-			
-			//Find the surface 
 			for(int i = 0; i < v.Length;i++)
 			{
-				//Debug.Log("No." + i + " = " + v[i]);
-				double n = (-1*Vector3.Dot(v[i] - movementDirection,hit.normal));
-				//Debug.Log(n);
-				
-				if(n < -0.01)
+				float n = Vector3.Dot(v[i] - movementDirection, hit.normal);
+			
+				if(n > 0.01f) 
 				{
+					
 					movementDirection.y = 0;
+
 					break;
 				}
 			}
-		}*/
-		
-		/*if(forward != 0)
+		}
+
+		if(Physics.Raycast(rightRay, out hit, dist, colMask,QueryTriggerInteraction.Ignore))
 		{
-			if(Physics.Raycast(frontRay, out hit, distY, colMask))
+			MeshCollider meshCollider = hit.collider as MeshCollider;		
+			Mesh mesh = meshCollider.sharedMesh;
+			Vector3[] v = mesh.vertices;
+
+			for(int i = 0; i < v.Length;i++)
 			{
-				MeshCollider meshCollider = hit.collider as MeshCollider;
-				if (meshCollider == null || meshCollider.sharedMesh == null)
-				{	
-					return;
-				}
-				
-				Mesh mesh = meshCollider.sharedMesh;
-				Vector3[] v = mesh.vertices;
-			
-				//Find the surface 
-				for(int i = 0; i < v.Length;i++)
+				float n = Vector3.Dot(v[i] - movementDirection, hit.normal);
+
+				if(n > 0.01f || n < 0.01f)
 				{
-					//Debug.Log("No." + i + " = " + v[i]);
-					double n = (-1*Vector3.Dot(v[i] - movementDirection,hit.normal));
-					
-					if(n < -0.01 || n > -0.01)
-					{
-						if(forward > 0)
-						{
-							movementDirection.z = 0;
-							break;
-						}
-					}
+					//Debug.Log("front="+n);
+
+					movementDirection.x = 0;
+					break;
 				}
-			}
-			
-			
-			if(Physics.Raycast(backRay, out hit, distZ, colMask))
-			{
-				MeshCollider meshCollider = hit.collider as MeshCollider;
-				if (meshCollider == null || meshCollider.sharedMesh == null)
-				{	
-					return;
-				}
-				
-				Mesh mesh = meshCollider.sharedMesh;
-				Vector3[] v = mesh.vertices;
-			
-				//Find the surface 
-				for(int i = 0; i < v.Length;i++)
-				{
-					//Debug.Log("No." + i + " = " + v[i]);
-					double n = (-1*Vector3.Dot(v[i] - movementDirection,hit.normal));
-					
-					if(n < -0.01 || n > -0.01)
-					{
-						if(forward < 0)
-						{
-							movementDirection.z = 0;
-							break;
-						}
-					}
-				}
-			}
+			}		
 		}
 		
-		if(right != 0)
+		if(Physics.Raycast(frontRay, out hit, dist+0.05f, colMask,QueryTriggerInteraction.Ignore))
 		{
-			if(Physics.Raycast(rightRay, out hit, distX, colMask))
-			{
-				MeshCollider meshCollider = hit.collider as MeshCollider;
-				if (meshCollider == null || meshCollider.sharedMesh == null)
-				{	
-					return;
-				}
-				
-				Mesh mesh = meshCollider.sharedMesh;
-				Vector3[] v = mesh.vertices;
-			
-				//Find the surface 
-				for(int i = 0; i < v.Length;i++)
-				{
-					//Debug.Log("No." + i + " = " + v[i]);
-					double n = (-1*Vector3.Dot(v[i] - movementDirection,hit.normal));
-					
-					if(n < -0.01 || n > -0.01)
-					{
-						if(right > 0)
-						{
-							movementDirection.x = 0;
-							break;
-						}
-					}
-				}
-			}
-			
-			if(Physics.Raycast(leftRay, out hit, distX, colMask))
-			{
-				MeshCollider meshCollider = hit.collider as MeshCollider;
-				if (meshCollider == null || meshCollider.sharedMesh == null)
-				{	
-					return;
-				}
-				
-				Mesh mesh = meshCollider.sharedMesh;
-				Vector3[] v = mesh.vertices;
-			
-				//Find the surface 
-				for(int i = 0; i < v.Length;i++)
-				{
-					//Debug.Log("No." + i + " = " + v[i]);
-					double n = (-1*Vector3.Dot(v[i] - movementDirection,hit.normal));
-					//print(n);
-					if(n < -0.01 || n > -0.01)
-					{
-						if(right < 0)
-						{
-							//print("test");
-							movementDirection.x = 0;
-							break;
-						}
-					}
-				}
-			}
-		}*/
-			
-			
-		return;
+			MeshCollider meshCollider = hit.collider as MeshCollider;		
+			Mesh mesh = meshCollider.sharedMesh;
+			Vector3[] v = mesh.vertices;
 
-		/*else
-		{
-			Debug.DrawLine(downRay.origin,downRay.direction*100,Color.green);
-		}*/
-	}	
+			for(int i = 0; i < v.Length;i++)
+			{
+				float n = Vector3.Dot(v[i] - movementDirection, hit.normal);
+
+				if(n > 0.01f || n < 0.01f)
+				{
+					movementDirection.z = 0;
+					break;
+				}
+			}		
+		}
 	
+		//--------------------------
+		// Collision correction for "wall-hugging", etc.
+		//--------------------------
+	
+		if(Physics.Raycast(uRightRay, out hit, dist, colMask,QueryTriggerInteraction.Ignore))
+		{
+			MeshCollider meshCollider = hit.collider as MeshCollider;		
+			Mesh mesh = meshCollider.sharedMesh;
+			Vector3[] v = mesh.vertices;
+
+			for(int i = 0; i < v.Length;i++)
+			{
+				float n = Vector3.Dot(v[i] - movementDirection, hit.normal);
+
+				if(n > 0.01f || n < 0.01f)
+				{
+					if(hit.distance < dist)
+					{
+						transform.position = Vector3.Lerp(transform.position, hit.point + (-transform.right) * dist, posRecover * Time.fixedDeltaTime);
+					}
+					
+					break;
+				}
+			}		
+		}
+		
+		if(Physics.Raycast(uLeftRay, out hit, dist, colMask,QueryTriggerInteraction.Ignore))
+		{
+			MeshCollider meshCollider = hit.collider as MeshCollider;		
+			Mesh mesh = meshCollider.sharedMesh;
+			Vector3[] v = mesh.vertices;
+
+			for(int i = 0; i < v.Length;i++)
+			{
+				float n = Vector3.Dot(v[i] - movementDirection, hit.normal);
+
+				if(n > 0.01f || n < 0.01f)
+				{
+					if(hit.distance < dist)
+					{
+						transform.position = Vector3.Lerp(transform.position, hit.point + (transform.right) * dist, posRecover * Time.fixedDeltaTime);
+					}
+					
+					break;
+				}
+			}		
+		}
+		
+		//------------------------
+	}
 		
 	void Update()
 	{
@@ -429,29 +364,37 @@ public class MouseAimC : MonoBehaviour
 		Ray front = new Ray (transform.position, transform.forward);
 		Debug.DrawLine(front.origin,front.direction*60,Color.blue);
 		//------
-	
+		
+
+		
+
 		cameraView.position = new Vector3(transform.position.x,transform.position.y+cameraOffsetY,transform.position.z);
 	}
 	
+	static float t = 0.0f;
 	
     void FixedUpdate()
     {
 		movementDirection = Vector3.zero;
 		
 		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+
 		
 		MoveWalk(input.y,input.x);
 		
-		//up vector manipulation (debug-only)
+
 		if(Input.GetKey(KeyCode.Space))
 		{
-			movementDirection.y = moveSpeed;
-		}
-		
-		Collision(ref movementDirection, input.y,input.x);
-		
-		//Debug.Log(input.x);
 
+			movementDirection.y += jumpSpeed;
+			
+			isGrounded = false;
+			//Move(movementDirection * Time.fixedDeltaTime);
+		}
+
+		movementDirection.y -= gravity;
+			
+		Collision(ref movementDirection, input.y,input.x);
 
 		//final transform calculation
 		Move(movementDirection * Time.fixedDeltaTime);	
