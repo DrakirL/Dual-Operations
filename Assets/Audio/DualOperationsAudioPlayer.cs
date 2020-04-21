39bc2f119e01e500a66c724b5569b249cef95cae
DrakirL <rikard.nt.larsson@hotmail.com>
2020-04-21 09:52:02 +0200

    Grunderna är satta.
    Testning krävs.
    Faktisk application finns inte än.

---
+++ Assets/Audio/DualOperationsAudioPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DualOperationsAudioPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void PlaySound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+    }
+}
