5bf5d1ed6bce343564fdcaaada320e8d0c471267
achieuw <47212392+achieuw@users.noreply.github.com>
2020-04-24 12:06:42 +0200

    states

--- Assets/Interaction/Scripts/Generator.cs
+++ Assets/Interaction/Scripts/Generator.cs
@@ -6,15 +6,18 @@ public class Generator : MonoBehaviour, IInteractable
 
     private void Start()
     {
-        activated = false;
+        activated = false;     
     }
 
     public void GetInteracted()
     {
         // Do generator stuff
         // Eventually use bool in another script to do generator stuff
-
-        activated = true;    
+        if (!activated)
+        {
+            GameManager._instance.WinState();
+            activated = true;
+        } 
     }
 
     public bool IsActivated() => (activated) ? true : false;
