bd1bc0e4856e5f2dba1020dae371dadd87e28ea9
DrakirL <rikard.nt.larsson@hotmail.com>
2020-04-24 14:08:55 +0200

    Patchwork-Lösning
    Jag tror att musiken ska fungera nu.
    Behöver testning från annan maskin.

--- Assets/Audio/DualOperationsAudioPlayer.cs
+++ Assets/Audio/DualOperationsAudioPlayer.cs
@@ -15,12 +15,15 @@ public class DualOperationsAudioPlayer : MonoBehaviour
         audioPlayer = this;
     }
 
+    //[Header("Music Settings")]
+
     // MANAGING MUSIC
-    private static FMOD.Studio.EventInstance Music;
+    private FMOD.Studio.EventInstance Music;
     public string musicPath;
 
-    private AlertMeter alert;
+    public AlertMeter alert;
 
+    /* Track system
     [System.Serializable]
     public class Track
     {
@@ -30,6 +33,7 @@ public class DualOperationsAudioPlayer : MonoBehaviour
     }
 
     [SerializeField] Track[] tracks;
+    */
 
     void Start()
     {
@@ -37,18 +41,21 @@ public class DualOperationsAudioPlayer : MonoBehaviour
         Music.start();
         Music.release();
 
-        alert = (AlertMeter)FindObjectOfType(typeof(AlertMeter));
+        if (alert != null)
+        {
+            Music.setParameterByName("RadioState", 0.0f);
+        }
     }
 
     void Update()
     {
-        if (alert != null)
+        if(alert != null)
         {
-            for(int i = 0; i < tracks.Length; i++)
-            {
-                Music.setParameterByName(tracks[i].parameterName, (Mathf.SmoothStep(0.0f, 1.0f, (tracks[i].fadeFloor - alert.alertValue) / (tracks[i].fadeFloor - tracks[i].fadeCeiling))));
-            }
+            Music.setParameterByName("LVL", alert.alertValue / 100.0f);
         }
+
+        //if tracks are used
+        //Music.setParameterByName(tracks[i].parameterName, (Mathf.SmoothStep(0.0f, 1.0f, (tracks[i].fadeFloor - alert.alertValue) / (tracks[i].fadeFloor - tracks[i].fadeCeiling))));
     }
 
     void OnDestroy()
@@ -56,7 +63,12 @@ public class DualOperationsAudioPlayer : MonoBehaviour
         Music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
 
+
+    //[Header("SFX Settings")]
     // MANAGING OTHER SOUNDS
+    public void HackCameraEvent()
+    { }
+
     void PlaySound(string path, GameObject source)
     {
         FMODUnity.RuntimeManager.PlayOneShot(path, source.transform.position);
