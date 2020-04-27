802e36911e4a476b3366d9952391dcc2800bf64d
Ante435 <ante435@hotmail.com>
2020-04-27 12:00:02 +0200

    xd

---
+++ Assets/taser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class taser : MonoBehaviour
+{
+
+    public Camera aimCam;
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("skjut");
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(aimCam.transform.position, aimCam.transform.forward, out hit, range))
+        {
+            AI target = hit.transform.GetComponent<AI>();
+            Debug.Log(target);
+            if (target != null)
+            {
+                target.dead = true;
+                Debug.Log("xd");
+            }
+        }
+    }
+}
