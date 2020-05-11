9a6e61bdf22b8a48b731eec1e3a04fd140cb2096
Elin Hallne <elin.hallne@hotmail.com>
2020-05-11 11:29:31 +0200

    First person

---
+++ Assets/Scripts/CheatSetActiveKeyCard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatSetActiveKeyCard : MonoBehaviour
+{
+
+    [SerializeField] public Collider activeCollider;
+    [SerializeField] public GameObject relatedKeyCard;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I´m in!");
+        if (Input.GetKey("e"))
+        {
+            relatedKeyCard.SetActive(true);
+            Debug.Log("I´m too far in");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
