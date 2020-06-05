0025badfff2e9c4951b14db559b61521803a92b1
achieuw <47212392+achieuw@users.noreply.github.com>
2020-06-05 17:33:34 +0200

    cutscene

---
+++ Assets/Cutscene2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Cutscene2 : MonoBehaviour
+{
+    VideoPlayer videoPlayer;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+    private void Start()
+    {
+        PlayCutscene();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EndCutscene(videoPlayer);
+        }
+
+    }
+    public void PlayCutscene()
+    {
+        //pause timer
+        videoPlayer.Play();
+        videoPlayer.loopPointReached += EndCutsceneEvent;
+    }
+    void EndCutsceneEvent(VideoPlayer vp)
+    {
+        EndCutscene(vp);
+    }
+
+    void EndCutscene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
