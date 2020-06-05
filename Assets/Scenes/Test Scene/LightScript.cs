76ee2a3d7491fb776337dfa1d591d288f67d44ba
Elin Hallne <elin.hallne@hotmail.com>
2020-06-05 19:03:31 +0200

    light

---
+++ Assets/Scenes/Test Scene/LightScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+
+   
+
+    void Start()
+        {
+            InvokeRepeating("MakeVisible", 0.0f, 0.5f);
+        }
+
+        void MakeVisible()
+        {
+            if (gameObject.activeSelf == false)
+            {
+                if (Camera.current != null)
+                {
+                    Vector3 pos = Camera.current.WorldToViewportPoint(transform.position);
+                    if (pos.z > 0 && pos.x >= 0.0f && pos.x <= 1.0f && pos.y >= 0.0f && pos.y <= 1.0f)
+                    {
+                        if (isDebug) Debug.Log(pos.ToString("F4"));
+                        gameObject.SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+
