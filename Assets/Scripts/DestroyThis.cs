5bfafc54ef804b75b49e04aeece98b9bc1520416
Elin Hallne <elin.hallne@hotmail.com>
2020-06-06 18:28:17 +0200

    destrothis

---
+++ Assets/Scripts/DestroyThis.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyThis : MonoBehaviour, IInteractable
+{
+    public GameObject gen0;
+
+    public void GetInteracted(List<int> io)
+    {
+        gen0.SetActive(false);
+        Debug.Log("Working");
+    }
+}
