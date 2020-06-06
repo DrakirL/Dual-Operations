35fcceeeb834e3a172403a24017184b8bb570868
DrakirL <rikard.nt.larsson@hotmail.com>
2020-06-06 11:31:51 +0200

    Enemy footsteps

--- Assets/Audio/DualOperationsAudioPlayer.cs
+++ Assets/Audio/DualOperationsAudioPlayer.cs
@@ -60,7 +60,7 @@ public class DualOperationsAudioPlayer : NetworkBehaviour
     [SerializeField] private string detectedPath;
     [SerializeField] private string[] hackingPaths;
     [SerializeField] private string tasorPath;
-    [SerializeField] private string footstepPath;
+    [SerializeField] private string[] footstepPaths;
     [SerializeField] private string[] doorPaths;
 
     public void Detected()
@@ -81,8 +81,11 @@ public class DualOperationsAudioPlayer : NetworkBehaviour
 
     public void Step(GameObject source)
     {
-        UnityEngine.Debug.Log("Step! " + isServer);
-        PlaySound(footstepPath, source.transform.position, Listerners.Agent);
+
+        int x = 0;
+
+        UnityEngine.Debug.Log(x + "step " + isServer);
+        PlaySound(footstepPaths[x], source.transform.position, Listerners.Agent);
     }
 
     public void Door(bool open, GameObject source)
