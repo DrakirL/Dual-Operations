04aebcb463e69dc96a75394f9eccef944d2f8645
Arilon_ <arvid.fredlun@gmail.com>
2020-06-06 14:39:27 +0200

    push

--- Assets/Interaction/Scripts/WinDocument1.cs
+++ Assets/Interaction/Scripts/WinDocument1.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Collections;
 public class WinDocument1 : MonoBehaviour, IInteractable
 {
     public void GetInteracted(List<int> io)
