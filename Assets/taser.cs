7457bff12d2e699800b5afcf2e26f9793063a982
Ekkara <47211128+Ekkara@users.noreply.github.com>
2020-06-06 17:51:24 +0200

    asdasdad

--- Assets/taser.cs
+++ Assets/taser.cs
@@ -19,15 +19,14 @@ public class taser : NetworkBehaviour
     [SerializeField] AnimationClip Shoot;
     [SerializeField] AnimationClip Reload;
     //[SerializeField] Transform lightStartPos;
-    [SerializeField] GameObject laserDisplay;
+    [SerializeField] GameObject laserVFX;
+    [SerializeField] Transform laserVFXspawnTransform;
     [Range(0,2)]
     [SerializeField] float laserDisplayTime = 0.3f;
-    GameObject laserParent;
 
     // Start is called before the first frame update
     void Start()
     {
-        laserParent = laserDisplay.transform.parent.gameObject;
         //this is the time of the animation
        // reloadTime = 2.633f;
     }
@@ -61,7 +60,6 @@ public class taser : NetworkBehaviour
                     agent.changeFPanimationState("SPY_SHOOT");
                     agent.changeAnimationStateState("SHOOT");
                     StartCoroutine(Cooldown(Shoot.length + Reload.length));
-                    //StartCoroutine(shootNoMoreAmmoAfter(0.633f));
                 }
             }
         }
@@ -83,48 +81,11 @@ public class taser : NetworkBehaviour
     private IEnumerator Cooldown(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        /*if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
-        {
-            agent.changeFPanimationState("SPY_WALK");
-            agent.changeAnimationStateState("WALK");
-        }
-        else
-        {
-            agent.changeFPanimationState("SPY_IDLE");
-            agent.changeAnimationStateState("IDLE");
-        }*/
+    
         tasorReady = true;
     }
-    /*private IEnumerator Cooldown(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
-        {
-            agent.changeFPanimationState("SPY_WALK");
-            agent.changeAnimationStateState("WALK");
-        }
-        else
-        {
-            agent.changeFPanimationState("SPY_IDLE");
-            agent.changeAnimationStateState("IDLE");
-        }
-        tasorReady = true;
-    }
-    private IEnumerator shootNoMoreAmmoAfter(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
-        {
-            agent.changeFPanimationState("SPY_WALK");
-            agent.changeAnimationStateState("WALK");
-        }
-        else
-        {
-            agent.changeFPanimationState("SPY_IDLE");
-            agent.changeAnimationStateState("IDLE");
-        }
-    }*/
 
+    [SyncVar] GameObject uslessTrash;
     [Command]
     public void CmdShoot()
     {
@@ -132,7 +93,6 @@ public class taser : NetworkBehaviour
         RaycastHit hit;
         if (Physics.Raycast(aimCam.transform.position, aimCam.transform.forward, out hit, range))
         {
-            RpcDispalyBeam(hit.point);
             AI target = hit.transform.GetComponent<AI>();
             Debug.Log(target);
             if (target != null)
@@ -140,27 +100,24 @@ public class taser : NetworkBehaviour
                 target.dead = true;
             }
         }
-    }
 
-   //GameObject spawnedBeam;
-    [ClientRpc]
-    private void RpcDispalyBeam(Vector3 point)
-    {
-        laserDisplay.SetActive(true);
-     //   GameObject spawnedBeam = Instantiate(laserDisplay, lightStartPos);
-        LineRenderer LR = laserDisplay.transform.GetChild(0).gameObject.GetComponent<LineRenderer>();
-        //LR.SetPosition(0, lightStartPos.position);
-        LR.SetPosition(1, point);
+        //create VFX
+        if (uslessTrash != null)
+        {
+            NetworkServer.UnSpawn(uslessTrash);
+        }
+        GameObject GO = Instantiate(laserVFX, laserVFXspawnTransform.position, laserVFXspawnTransform.rotation );
+        NetworkServer.Spawn(GO);
+        uslessTrash = GO;
         StartCoroutine(destroyBeam());
-        laserDisplay.transform.parent = null;
-
     }
+
     IEnumerator destroyBeam()
     {
         yield return new WaitForSeconds(laserDisplayTime);
-        laserDisplay.SetActive(false);
-        laserDisplay.transform.parent = laserParent.transform;
-        laserDisplay.transform.localPosition = Vector3.zero;
-        //  Destroy(spawnedBeam);
+        if (uslessTrash != null)
+        {
+            NetworkServer.UnSpawn(uslessTrash);
+        }
     }
 }
